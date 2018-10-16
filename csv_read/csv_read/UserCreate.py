c9b8ee4fb3aef32a64db929ac0019b0badd01897
Alan Gomes <alanhr2@gmail.com>
2018-10-16 11:59:51 -0300

    changing function name

--- csv_read/csv_read/UserCreate.py
+++ csv_read/csv_read/UserCreate.py
@@ -1,14 +1,14 @@
 from csv_read.model.User import User
 
 
-def __is_exists(user):
+def __user_exists(user):
     user_find = User.objects(email=user.get('email')).first()
     
     return True if user_find else False
 
 
 def create(user):
-    if __is_exists(user):
+    if __user_exists(user):
         raise Exception('this email {} was already registered'.format(
             user.get('email')
         ))
